dec78827e80ca2478c97ef7a911c76d69805ba16
Simon Rolph <simon.p.rolph@gmail.com>
2017-06-15 22:51:54 +0100

    updates

--- builders/website_builder/constructor.R
+++ builders/website_builder/constructor.R
@@ -6,14 +6,12 @@ for (i in 1:length(files)){
   content <- readLines(paste("../pages/",files[i],sep=""))
   
   title <- content[1]
-  subtitle <- content[2]
-  content <- content[3:length(content)]
+  content <- content[2:length(content)]
   
   content <- paste(content, collapse = '')
   
   page <- gsub("INSERT_CONTENT", content, template)
   page <- gsub("INSERT_TITLE", title, page)
-  page <- gsub("INSERT_SUBTITLE", subtitle, page)
   
   write(page,file = paste("../../",files[i],sep = ""))
 }
