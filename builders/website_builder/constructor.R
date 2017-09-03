2b837f9d8369640ab7ce833ba3b2468b914383b1
unknown <simon.p.rolph@gmail.com>
2017-09-03 17:16:27 +0100

    big update

--- builders/website_builder/constructor.R
+++ builders/website_builder/constructor.R
@@ -1,17 +1,22 @@
 files <- list.files(path = "../pages/")
 
 
-for (i in 1:length(files)){
-  template <- readLines("../template.html")
-  content <- readLines(paste("../pages/",files[i],sep=""))
-  
-  title <- content[1]
-  content <- content[2:length(content)]
-  
-  content <- paste(content, collapse = '')
-  
-  page <- gsub("INSERT_CONTENT", content, template)
-  page <- gsub("INSERT_TITLE", title, page)
-  
-  write(page,file = paste("../../",files[i],sep = ""))
+for (ii in 1:1000000){
+  print("go")
+  for (i in 1:length(files)){
+    template <- readLines("../template.html")
+    content <- readLines(paste("../pages/",files[i],sep=""))
+    
+    title <- content[1]
+    content <- content[2:length(content)]
+    
+    content <- paste(content, collapse = '')
+    
+    page <- gsub("INSERT_CONTENT", content, template)
+    page <- gsub("INSERT_TITLE", title, page)
+    
+    write(page,file = paste("../../",files[i],sep = ""))
+    
+  }
+  Sys.sleep(3)
 }
